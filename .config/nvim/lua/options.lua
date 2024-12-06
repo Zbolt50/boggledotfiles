95e660e297bcb30b73914e48b2a832d44641c552
Zbolt50 <zekeboggs10@gmail.com>
2024-12-06 14:02:14 -0500

    added initial testing nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,13 @@
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
+vim.opt.expandtab = true            -- python. only reason this is here			i
+
+vim.opt.number = true
+vim.opt.incsearch = true            -- search as characters are entered
+vim.opt.hlsearch = false            -- do not highlight matches
+vim.opt.ignorecase = true           -- ignore case in searches by default
+vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('ThePrimeagen/vim-be-good')
+Plug('https://github.com/tpope/vim-fugitive')
+
+
+vim.call('plug#end')
